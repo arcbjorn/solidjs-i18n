1af321e677536461d126e9648739a9a22298d99f
arcbjorn <arky.happ@gmail.com>
2025-02-15 21:48:47 -0300

    chore: add eslint & prettier

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import solid from "eslint-plugin-solid";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      solid,
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...solid.configs.recommended.rules,
+      "solid/reactivity": "warn",
+      "solid/no-destructure": "warn",
+      "solid/jsx-no-undef": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+];
