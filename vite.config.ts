f1fbbc425764c6296f708b2dba3075756266f3b2
arcbjorn <arky.happ@gmail.com>
2025-02-15 22:52:08 -0300

    chore: prep for publish

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,21 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'solid-i18n',
+      fileName: (format) => `solid-i18n.${format}.js`,
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['solid-js', 'solid-js/store'],
+      output: {
+        globals: {
+          'solid-js': 'Solid',
+          'solid-js/store': 'SolidStore'
+        }
+      }
+    },
     target: 'esnext',
   },
 });
